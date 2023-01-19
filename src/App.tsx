c860180e742b4db6203f1de4e062dd3c208233c2
MafiaDon2011 <wajidhussain1@live.com>
2023-01-19 13:31:16 +0000

    Creating the multi-step hook.

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useMultistepForm } from "./hooks/useMultistepForm"
+
+function App() {
+  const { steps, currentStepIndex } = useMultistepForm([<div>One</div>, <div>Two</div>])
+  const {} = useMultistepForm(steps)
+  return <div style={{
+    position: "relative",
+    background: "white",
+    border: "1px solid black",
+    padding: "2rem",
+    margin: "1rem",
+    borderRadius: ".5rem",
+    fontFamily: "Arial"
+  }}>
+    <form>
+      <div style={{
+        position: "absolute",
+        top: ".5rem",
+        right: ".5rem",
+      }}>
+        {currentStepIndex + 1} / {steps.length}
+        
+      </div>
+    </form>
+  </div>
+}
+
+export default App
